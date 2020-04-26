a7142c05fe306b1b08f510953db8cc3dd826a166
Adriano Maciel <adriano_mail@hotmail.com>
2020-04-26 07:59:28 -0300

    Desativada saía CLI durante testes do bootstrap

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -9,12 +9,20 @@ final class Boostrap
 {
     public function run($sapi): void
     {
+        if (\PHPUNIT_TEST_IS_RUNNING) {
+            ob_start();
+        }
+
         if ($sapi === 'cli') {
             $f = new Cli();
             $f->dispatchInput();
         } else {
             $f = new Web();
             $f->render();
+        }
+
+        if (\PHPUNIT_TEST_IS_RUNNING) {
+            ob_get_clean();
         }
     }
 }
